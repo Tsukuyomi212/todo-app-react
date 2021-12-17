a9a7f7a145952576b974575b6b6eb3984a8b911a
Tsukuyomi212 <nada.zelic@gmail.com>
2021-12-17 10:12:56 +0100

    refactor

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContextProvider';
+import { LOGIN } from '../../utils/routes';
 import { Sidebar } from '../sidebar/Sidebar';
 import './homepage.css';
 
@@ -9,12 +10,22 @@ export const Homepage = () => {
   const navigate = useNavigate();
   const { authenticatedUser } = useContext(AuthContext);
 
+  useEffect(() => {
+    if (!authenticatedUser) {
+      navigate(LOGIN);
+    }
+  });
+
   return (
     <>
-      <Sidebar />
-      <div className="content">
-        <h1>Welcome to Todo App!</h1>
-      </div>
+      {authenticatedUser && (
+        <div>
+          <Sidebar />
+          <div className="content">
+            <h1>Welcome to Todo App!</h1>
+          </div>
+        </div>
+      )}
     </>
   );
 };
